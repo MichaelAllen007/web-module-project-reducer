7d3308a4a3c9d32daf22d971baf93cb65783c94a
unknown <allenmk20073@yahoo.com>
2021-03-24 11:26:54 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+import reducer, { initialState } from './reducers/index';
+import { addOne, applyNumber, clearDisplay, changeOperation, mPlus, mr, mc} from './actions/index'
 
 import './App.css';
 
@@ -6,6 +8,15 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const eventHandler = (callback, value = null) => {
+    if (value) {
+      return () => dispatch(callback(value))
+    } else {
+      return () => dispatch(callback())
+    }
+  }
 
   return (
     <div className="App">
@@ -17,44 +28,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation: </b>{state.operation}</span>
+              <span id="memory"><b>Memory:</b>{state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton value={"M+"} onClick={eventHandler(mPlus)}/>
+              <CalcButton value={"MR"} onClick={eventHandler(mr)}/>
+              <CalcButton value={"MC"} onClick={eventHandler(mc)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={eventHandler(applyNumber , 1)}/>
+              <CalcButton value={2} onClick={eventHandler(applyNumber , 2)}/>
+              <CalcButton value={3} onClick={eventHandler(applyNumber , 3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={eventHandler(applyNumber , 4)}/>
+              <CalcButton value={5} onClick={eventHandler(applyNumber , 5)}/>
+              <CalcButton value={6} onClick={eventHandler(applyNumber , 6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={eventHandler(applyNumber , 7)}/>
+              <CalcButton value={8} onClick={eventHandler(applyNumber , 8)}/>
+              <CalcButton value={9} onClick={eventHandler(applyNumber , 9)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={eventHandler(changeOperation, '+')}/>
+              <CalcButton value={"*"} onClick={eventHandler(changeOperation, '*')}/>
+              <CalcButton value={"-"} onClick={eventHandler(changeOperation, '-')}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={eventHandler(clearDisplay)}/>
             </div>
 
           </form>
